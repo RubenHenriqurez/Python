f21a9e05b88727a9dd4c879d530194823fd4a40f
Ruben Henriquez <rubenhenriquez555@gmail.com>
2026-05-13 12:10:44 -0500

    Formas de formatear los strings en python

---
+++ tipos/03-format-strings.py
@@ -0,0 +1,8 @@
+nombre = "Rubén"
+apellido = "Henríquez"
+#nombre_completo = nombre + " " + apellido--Este es el primer metodo, pero hay uno mejor
+
+#nombre_completo = f"{nombre} {apellido}"#formateo de estring, lo que esta comentado son diferentes metodos
+
+nombre_completo = f"{nombre[0]} {2 + 5}"
+print(nombre_completo)

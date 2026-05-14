19a6b7c71bbef3f99e2e6c9c14cfcedf18252162
Ruben Henriquez <rubenhenriquez555@gmail.com>
2026-05-14 12:53:18 -0500

    Comentarios salto de linea

---
+++ tipos/05-secuencias-escape.py
@@ -0,0 +1,9 @@
+#comenatrios
+# \"
+# \ '
+# \\
+# \n, lo pasa a una nueva linea salto de linea
+
+
+curso = "ultimate \npython\""#se utilizan para las comillas para diferenciar y que funcione
+print(curso)

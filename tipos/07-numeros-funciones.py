cbb95a957fdd46f311b0fa9ee77b1f6ea3b0b878
Ruben Henriquez <rubenhenriquez555@gmail.com>
2026-05-14 13:22:14 -0500

    Numero y funciones, funcioón math

---
+++ tipos/07-numeros-funciones.py
@@ -0,0 +1,25 @@
+print(round(1.3))# va a devolver al numero que estemos más sercano 
+print(round(1.7))
+print(round(1.5))
+
+print(abs(-77))#nos entrega el valor obsoluto del numero que le pasemos al valor 
+print(abs(55))
+
+#modulo  otro archivo del cual podemos extraer codigo en python para utilizar 
+import math#codigo escrito en python
+
+
+print(math.ceil(1.1))#lo llevz l numero superior más sercano 
+print(math.floor(1.99999))# va a tomar el numero independiente de cual sea y lo va allevar a el numero más sercano, pero hacia abajo 
+
+#print(math.isnan(23))#indentifica si es un número o no solo si pasamos numero reales
+
+print(math.pow(10, 3))# nos permite elevar un numero a la potencia de algo 
+print(math.sqrt(9))# saca la raiz cuadrada 
+
+#buscar math en el navegador para encontar todos los metodos
+
+
+
+
+

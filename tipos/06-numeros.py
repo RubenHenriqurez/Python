6238d6b1b90676b5473198e32cb21e9eb7a9af5c
Ruben Henriquez <rubenhenriquez555@gmail.com>
2026-05-14 13:07:23 -0500

    Números y operaciones

---
+++ tipos/06-numeros.py
@@ -0,0 +1,20 @@
+numero = 2 #entero->integer
+decimal = 1.2#float
+imaginario = 2 + 2j #2 + 2i numero imaginarios 
+
+
+#numero = numero + 2 # se remplza por si misma y se suma 2
+
+#numero += 2#esto suma lo de lz izquierda y se lo suma  a la misma varibale
+
+
+numero *= 5# se puede hacer con todas las operaciones 
+print("numero", numero)
+
+print(1 + 3)
+print(1 - 3)
+print(1 * 3)
+print(1 / 3)# nos da el numero con decimales
+print(1 // 3)# nos da el numero sin decimales entero
+print(8 % 3)#modulo es lo que sobra de la división se agrega un 0 
+print(2 ** 3)#elevar a 3 o como se necesite

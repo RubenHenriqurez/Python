b2260512df505b80ab6699bf0f3c7e81d04be8ae
Ruben Henriquez <rubenhenriquez555@gmail.com>
2026-05-15 14:01:14 -0500

    IF.ELIF, ELSE

---
+++ Control-flujo/02-if.py
@@ -0,0 +1,20 @@
+edad = 70
+
+#edad = 15
+#Las evaluaciones se hacen de arrib hacia abajo, si no pasa uno pasa al siguiente
+#Es importante el orden
+if edad > 65:
+
+    print("Puedes ver la pelicula con super descuento")#importante los espacios para ser considerados dentro del if
+
+elif edad > 54:
+    print("Puedes ver la pelicula con descuento")
+elif edad > 17:
+    print("Puedes ver la pelicula")
+else:
+    print("No puedes entrar")
+
+
+print("Listo")
+
+

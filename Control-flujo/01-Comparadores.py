0b1a5ec29dbe2acfa3979415cc585f4a6b8d1f50
Ruben Henriquez <rubenhenriquez555@gmail.com>
2026-05-15 13:46:19 -0500

    Comparadores <>=!

---
+++ Control-flujo/01-Comparadores.py
@@ -0,0 +1,20 @@
+print(1 > 2)#expresiones evalua ejemplo variables n1 +  n2 expresion dentro de los parenteesis siempre devuelve tru o flase
+
+print(1 < 2)
+
+print(1 <= 2)
+print(2 >= 2)
+
+print(2 <= 2)
+
+print(2 == 2)#se coloca  doble porque si no parece que le estamos asignando un dato a una varibale, para hacer la comparación de igualdad se debe colocar ==
+
+print(2 == 3)
+
+print(2 == "2")
+
+print(2 != "2")# No es igual desigualdad
+print(2 != 2)
+
+
+

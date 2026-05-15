11cfd7a1c751db103d3c1bc43a73701676e65c6c
Ruben Henriquez <rubenhenriquez555@gmail.com>
2026-05-15 14:12:06 -0500

    Forma diferente de escribir el if else

---
+++ Control-flujo/03-if-ternario.py
@@ -0,0 +1,12 @@
+edad = 19
+
+
+mensaje = "es mayor" if edad > 17 else "es menor" #se cambia como se hace
+
+
+# if edad > 17:
+#     mensaje = ("Es mayor")
+# else:
+#     mensaje = ("Es menor")
+
+print(mensaje)

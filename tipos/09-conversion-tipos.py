ab9c9253626a87e68b02937293a9665a47f36cec
Ruben Henriquez <rubenhenriquez555@gmail.com>
2026-05-15 13:27:32 -0500

    Conversiones booleanas etc

---
+++ tipos/09-conversion-tipos.py
@@ -0,0 +1,16 @@
+#x = input("")
+
+
+#Hace transformaciones
+#int()
+#str()
+#float()
+#bool(), ""espacio vacio-0, none, todo esto es basado en false.
+
+
+print(bool(""))
+print(bool("0"))
+print(bool(None))
+print(bool(" "))
+print(bool(0))
+

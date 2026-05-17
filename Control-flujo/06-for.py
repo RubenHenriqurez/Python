2489c80d4d8fe1254faf24fc77299cc378253c5f
Ruben Henriquez <rubenhenriquez555@gmail.com>
2026-05-17 13:39:59 -0500

    For con if-else

--- Control-flujo/06-for.py
+++ Control-flujo/06-for.py
@@ -1,5 +1,12 @@
-for numero in range(5):#crea una secuencia de numeros que devuelve un listado, 0,1,2,3,4. 
-    print(numero, numero  * 'Hola mundo' )
+Buscar = 10
 
 
+for numero in range(5):
+    print(numero)#crea una secuencia de numeros que devuelve un listado, 0,1,2,3,4. 
+    if numero == Buscar:
+        print("Encontrado", Buscar)
+        break# se usa para detener el ciclo
+
+else:
+    print(" no encontre el número buscado")
 #Itera una lista de elememtos, nombre y apellido=nombre completo, otro uso para buscar elementos y operaciones amtematicas

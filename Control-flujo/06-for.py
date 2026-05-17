9f28c9d0199c56334ce942abf1068cad961aebcd
Ruben Henriquez <rubenhenriquez555@gmail.com>
2026-05-17 13:31:46 -0500

    Introducción a for

---
+++ Control-flujo/06-for.py
@@ -0,0 +1,5 @@
+for numero in range(5):#crea una secuencia de numeros que devuelve un listado, 0,1,2,3,4. 
+    print(numero, numero  * 'Hola mundo' )
+
+
+#Itera una lista de elememtos, nombre y apellido=nombre completo, otro uso para buscar elementos y operaciones amtematicas

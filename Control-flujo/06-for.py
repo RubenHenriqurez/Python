d32a93a92461522e0cfd816bea8e6bcad171f5a7
Ruben Henriquez <rubenhenriquez555@gmail.com>
2026-05-17 13:43:53 -0500

    Iterables

--- Control-flujo/06-for.py
+++ Control-flujo/06-for.py
@@ -1,7 +1,7 @@
 Buscar = 10
 
 
-for numero in range(5):
+for numero in range(5):#Iterable, cualquier cosa que sea iterable, listas, tuplas todas son iterables, strings
     print(numero)#crea una secuencia de numeros que devuelve un listado, 0,1,2,3,4. 
     if numero == Buscar:
         print("Encontrado", Buscar)
@@ -10,3 +10,7 @@ for numero in range(5):
 else:
     print(" no encontre el número buscado")
 #Itera una lista de elememtos, nombre y apellido=nombre completo, otro uso para buscar elementos y operaciones amtematicas
+
+for char in "Ultimte python":
+    print(char)
+

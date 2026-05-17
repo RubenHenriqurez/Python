c897b32c6fd53399808f226016c36e5e61b0c894
Ruben Henriquez <rubenhenriquez555@gmail.com>
2026-05-17 13:54:05 -0500

    While funcionamiento

---
+++ Control-flujo/07-while.py
@@ -0,0 +1,20 @@
+# numero = 1
+# while numero < 100:# Evalua lo que esta a la derecha  imprime el numero y luego multiplica, que pasa en la ultimaiteraciones tenemos 128 es menor que 100 es hay donde la ejecucion edl while se detiene
+#     print(numero)
+#     numero *= 2
+
+
+
+
+comando = ""
+
+while comando.lower != "salir":
+    comando = input("$")
+    print(comando)
+
+
+
+
+
+
+
